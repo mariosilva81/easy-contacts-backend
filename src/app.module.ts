9603ac8cfe665df7ce9407e7a31773fcaa37f652
Mario Silva <mariosilva.81@icloud.com>
2023-11-14 11:53:00 -0300

    feat: create clients and contacts modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClientsModule } from './modules/clients/clients.module';
+import { ContactsModule } from './modules/contacts/contacts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ClientsModule, ContactsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
